fca845073eaccc367432b56119703c743db1a024
BigDaddy <singla_rohan@yahoo.co.in>
2011-07-13 18:18:12 +0100

    rsion 2.0

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,2 +1,13 @@
 class Result < ActiveRecord::Base
+
+require 'rest-uri'
+  
+  after_save :notify_repository
+
+  def notify_repository
+    
+    RestClient.post (http://127.0.0.1:3001/websites,to_json)
+  end
+
+
 end
